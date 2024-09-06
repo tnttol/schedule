956c6907f754a8a9f1bb0bb5e315977a53db8884
Anatoliy Beketov <anatolii.beketov@idapgroup.com>
2024-09-06 16:20:33 +0300

    v1

--- src/Base/Day/Day5.php
+++ src/Base/Day/Day5.php
@@ -10,7 +10,6 @@ use App\Base\Lesson\Lesson4;
 use App\Base\Lesson\Lesson5;
 use App\Base\Lesson\Lesson6;
 use App\Base\Lesson\Lesson7;
-use App\Base\Lesson\Lesson8;
 use App\Base\Subject\Drawing;
 use App\Base\Subject\EnglishLanguage;
 use App\Base\Subject\Informatics;
@@ -18,7 +17,6 @@ use App\Base\Subject\Mathematics;
 use App\Base\Subject\NaturalScience;
 use App\Base\Subject\UkrainianLanguage;
 use App\Base\Subject\UkrainianLiterature;
-use App\Base\Subject\Undefined;
 
 class Day5 extends AbstractDay implements DayInterface
 {
@@ -32,7 +30,6 @@ class Day5 extends AbstractDay implements DayInterface
             ->addLesson((new Lesson5())->addSubject(new Mathematics()))
             ->addLesson((new Lesson6())->addSubject(new Drawing()))
             ->addLesson((new Lesson7())->addSubject(new NaturalScience()))
-            ->addLesson((new Lesson8())->addSubject(new Undefined()))
         ;
     }
 }
