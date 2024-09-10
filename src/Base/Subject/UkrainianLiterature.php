98230b91d24868229dbc241bdcd3e6ed709a157e
Anatoliy Beketov <anatolii.beketov@idapgroup.com>
2024-09-10 11:43:33 +0300

    v1

--- src/Base/Subject/UkrainianLiterature.php
+++ src/Base/Subject/UkrainianLiterature.php
@@ -3,13 +3,13 @@
 namespace App\Base\Subject;
 
 use App\Base\SubjectInterface;
-use App\Base\Teacher\Akimova;
+use App\Base\Teacher\Unknown;
 
 class UkrainianLiterature extends AbstractSubject implements SubjectInterface
 {
     public function __construct()
     {
-        $this->addTeacher(new Akimova());
+        $this->addTeacher(new Unknown());
     }
 
     public function getSubjectName(): string
